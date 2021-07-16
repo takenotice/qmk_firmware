42ba35d4aa0453a13453cbd9ad3efc1d3b383299
takenotice <codyrobot@gmail.com>
2021-07-16 17:33:50 -0400

    Fix qwerty layout bug

--- keyboards/kingly_keys/ropro/keymaps/takenotice/keymap.c
+++ keyboards/kingly_keys/ropro/keymaps/takenotice/keymap.c
@@ -57,13 +57,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------------------------------------------------'
    */
   	[QRTY] = LAYOUT(
-              KC_ESC,   KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,   KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,
-              KC_EQL,   KC_1,    KC_2,    KC_3,    KC_4,     KC_5,    KC_6,    KC_7,     KC_8,    KC_9,    KC_0,    KC_MINUS,
-    KC_MUTE,  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,    KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,    KC_BSPC,
-    DF(QRTY), KC_CAPS,  KC_A,    KC_S,    KC_D,    KC_F,     KC_G,    KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    DF(COMK), KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,    KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH, _RSH_EN,
-    _MEH,     KC_LCTRL, KC_DEL,  KC_LGUI, KC_LALT, MO(FUNC), KC_SPC,  KC_SPC,  MO(FUNC), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+              KC_ESC,   KC_F1,    KC_F2,   KC_F3,   KC_F4,    KC_F5,   KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,
+              KC_EQL,   KC_1,     KC_2,    KC_3,    KC_4,     KC_5,    KC_6,    KC_7,     KC_8,    KC_9,    KC_0,    KC_MINUS,
+    KC_MUTE,  KC_TAB,   KC_Q,     KC_W,    KC_E,    KC_R,     KC_T,    KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,    KC_BSPC,
+    DF(QRTY), KC_CAPS,  KC_A,     KC_S,    KC_D,    KC_F,     KC_G,    KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    DF(COMK), KC_LSFT,  KC_Z,     KC_X,    KC_C,    KC_V,     KC_B,    KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH, _RSH_EN,
+    _MEH,     KC_LCTRL, KC_DEL,   KC_LGUI, KC_LALT, MO(FUNC), KC_SPC,  KC_SPC,  MO(FUNC), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
   ),
+
+  // 	[_BASE] = LAYOUT(
+  //             KC_ESC,   KC_F1,    KC_F2,   KC_F3,   KC_F4,    KC_F5,   KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,
+  //             KC_EQL,   KC_1,     KC_2,    KC_3,    KC_4,     KC_5,    KC_6,    KC_7,     KC_8,    KC_9,    KC_0,    KC_MINUS,
+  //   KC_MUTE,  KC_TAB,   KC_Q,     KC_W,    KC_E,    KC_R,     KC_T,    KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,    KC_BSPC,
+  //   DF(QRTY), KC_CAPS,  KC_A,     KC_S,    KC_D,    KC_F,     KC_G,    KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  //   DF(COMK), KC_LSFT,  KC_Z,     KC_X,    KC_C,    KC_V,     KC_B,    KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH, _RSH_EN,
+  //   _MEH,     KC_LCTRL, KC_DEL,   KC_LGUI, KC_LALT, MO(FUNC), KC_SPC,  KC_SPC,  MO(FUNC), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  // ),
+
+
     /* COMK - Colemak
    *        ,-----------------------------------------------------------------------------------.
    *        | Esc  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |
@@ -104,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |      |      |      |      |      |      |             |      |      |      |      |      |
    * `------------------------------------------------------------------------------------------'
    */
-    [SYMB] = LAYOUT(
+    [FUNC] = LAYOUT(
               _______,  KC_F11,   KC_F12,   KC_F13,   KC_F14,   KC_F15,   KC_F16,   KC_F17,   KC_F18,   KC_F19,   KC_F20,  KC_F21,
               KC_GRAVE, _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, _______,
     _______,  _______,  KC_EXLM,  KC_AT,    KC_LCBR,  KC_RCBR,  KC_PIPE,  _______,  KC_HOME,  KC_UP,    KC_END,   KC_PSCR, KC_BSLS,
